a79ea0042ee5e3075257076ad57318c58325ba7f
apangri <apangri@cisco.com>
2020-04-22 21:12:09 +0530

    Added mac validation, and README

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # RestTest
-Find details of a mac address.
+Finds details of a mac address.
+
+The details of the given Mac address are fetched from macaddress.io
+The given mac address is validated before sending the API request.
+API connection does not use requests/urllib packages of python.
+The API call is handled in a rudimentary HTTP client way.
+The API key required to access the details is stored in a separate config file, for security and easy management.
+
+Input: Mac Address.
+example:
+Please enter the macAddress:
+44:38:39:ff:ef:57
+
+Output: Company name and details.
+example:
+####################################################################
+Company Details:
+Cumulus Networks, Inc
+650 Castro Street, suite 120-245 Mountain View CA 94041 US
+####################################################################
+
+Though only company details are shown as output, All the details returned by the API are stored as a json file for future use.
+The program also generates a log file.
+
+Also, this python app can be dockerized, and can be run as an independant container.
+Steps to create and run the docker container:
+
+step 1:
+docker build -t <docker_image_name> .
+{to be run at the location where dockerfile is present}
+
+step 2:
+docker run -it <docker_image_name>
+{-i stands for interactive mode. Program expects the input from the User.}
--- resttest.py
+++ resttest.py
@@ -3,6 +3,7 @@ import ssl
 import json
 import config
 import logging
+import re
 
 httpClient = None
 apiKey = config.apiKey
@@ -15,31 +16,36 @@ if __name__ == "__main__":
                         format='%(name)s - %(levelname)s - %(message)s')
     print("Please enter the macAddress: ")
     macAddress = input()
-    logging.info('Finding the details of the given macAddress ' + macAddress)
+    # Validating the given mac address.
+    if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", macAddress.lower()):
+        logging.info("Validation passed.")
+        logging.info('Finding the details of the given macAddress ' + macAddress)
+        try:
+            logging.debug("Inside the try block")
+            httpClient = http.client.HTTPSConnection('api.macaddress.io')
+            logging.info("Opening an HTTPS connection with macaddress.io")
+            httpClient.request('GET', "/v1?apiKey={}&output=json&search={}".format(apiKey, macAddress))
+            response = httpClient.getresponse()
+            result_all = response.read().decode("utf-8")
+            result = json.loads(result_all)
+            logging.info("Response from the API:  \n" + str(result))
+            with open('data.txt', 'w') as outfile:
+                json.dump(result, outfile)
 
-    try:
-        logging.debug("Inside the try block")
-        httpClient = http.client.HTTPSConnection('api.macaddress.io')
-        logging.info("Opening an HTTPS connection with macaddress.io")
-        httpClient.request('GET', "/v1?apiKey={}&output=json&search={}".format(apiKey, macAddress))
-        response = httpClient.getresponse()
-        result_all = response.read().decode("utf-8")
-        result = json.loads(result_all)
-        logging.info("Response from the API:  \n" + str(result))
-        with open('data.txt', 'w') as outfile:
-            json.dump(result, outfile)
+            print("\n\n\n")
+            print("####################################################################")
+            print("Company Details: \n" + result['vendorDetails']['companyName'])
+            print(result['vendorDetails']['companyAddress'])
+            print("####################################################################")
 
-        print("\n\n\n")
-        print("####################################################################")
-        print("Company Details: \n" + result['vendorDetails']['companyName'])
-        print(result['vendorDetails']['companyAddress'])
-        print("####################################################################")
+        except Exception as e:
+            logging.info("Caught exception while fetching API response.\n" + str(e))
+            print(e)
 
-    except Exception as e:
-        logging.info("Caught exception while fetching API response.\n" + str(e))
-        print(e)
-
-    finally:
-        if httpClient:
-            httpClient.close()
-            logging.info("Closing the HTTPS connection with macaddress.io")
+        finally:
+            if httpClient:
+                httpClient.close()
+                logging.info("Closing the HTTPS connection with macaddress.io")
+    else:
+        logging.info("Given mac address {} is invalid.".format(macAddress))
+        print("\n\nGiven address {} is invalid. Please provide correct mac address.".format(macAddress))
